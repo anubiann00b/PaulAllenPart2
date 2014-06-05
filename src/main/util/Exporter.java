c66f04b6cae49956863babc1fbf1926634e38b67
QuantumPhi <phi.quantum@gmail.com>
2014-06-05 03:58:31 -0700

    Still working

--- src/main/util/Exporter.java
+++ src/main/util/Exporter.java
@@ -13,22 +13,22 @@ import main.util.data.Packet;
 
 /** Contains static methods to export Data[] */
 public class Exporter {
-    
+
     public static void exportTweetsOverTimeCsv(Data[] data, int hoursStep, String file, boolean time) throws IOException {
         int[] tweets = exportTweetsOverTime(data,hoursStep);
-        
+
         StringBuilder result = new StringBuilder();
-        
+
         Date current = (Date) data[0].time.clone();
-        
+
         int milliStep = hoursStep*3600000;
-        
+
         DateFormat df = new SimpleDateFormat("MM/dd HH:mm");
-        
+
         if (time)
             result.append("Time,");
         result.append("Tweets\n");
-        
+
         for (int i=0;i<tweets.length;i++) {
             if (time)
                 result.append(df.format(current)).append(",");
@@ -39,22 +39,22 @@ public class Exporter {
         out.print(result.toString());
         out.close();
     }
-    
+
     public static void exportTweetsOverTimeCsv(Data[] data, int hoursStep) throws IOException {
         exportTweetsOverTimeCsv(data,hoursStep,"tweets_over_time.csv",true);
     }
-    
+
     public static String exportTweetsOverTimeAscii(Data[] data, int hoursStep, int scale) {
         int[] tweets = exportTweetsOverTime(data,hoursStep);
-        
+
         StringBuilder result = new StringBuilder();
-        
+
         Date current = (Date) data[0].time.clone();
-        
+
         int milliStep = hoursStep*3600000;
-        
+
         DateFormat df = new SimpleDateFormat("MM/dd HH:mm");
-        
+
         for (int i=0;i<tweets.length;i++) {
             result.append(df.format(current)).append(" - ");
             current.setTime(current.getTime()+milliStep);
@@ -65,20 +65,20 @@ public class Exporter {
         }
         return result.toString();
     }
-    
+
     public static int[] exportTweetsOverTime(Data[] data, int hoursStep) {
         data = Sorter.sortByTime(data);
-        
+
         Date current = (Date) data[0].time.clone();
-        
+
         int milliStep = hoursStep*3600000;
-        
+
         int pos = -1;
-        
+
         long duration = data[data.length-1].time.getTime()-data[0].time.getTime();
-        
+
         int[] results = new int[(int)(duration/milliStep)+1];
-        
+
         for (Data item : data) {
             if (!item.time.before(current)) {
                 current.setTime(current.getTime()+milliStep);
@@ -86,40 +86,62 @@ public class Exporter {
             }
             results[pos]++;
         }
-        
+
         return results;
     }
-    
+
+    public static Data[][] exportRawTweetsOverTime(Data[] data, int hoursStep) {
+        data = Sorter.sortByTime(data);
+
+        Date current = (Date) data[0].time.clone();
+
+        int millistep = hoursStep * 3600000;
+
+        int pos = -1;
+
+        long duration = data[data.length - 1].time.getTime()-data[0].time.getTime();
+        Data[][] results = new Data[(int)(duration/milliStep)+1][1];
+
+        for(Data item : data) {
+            if(!item.time.before(current)) {
+                current.setTime(current.getTime() + milliStep);
+                pos++;
+            }
+
+        }
+    }
+
     public static Packet[] exportToPackets(Data[] data) {
         Packet[] returnPacket = new Packet[data.length];
-        
+
         for(int i = 0; i < data.length; i++)
             returnPacket[i] = Packet.getPacketFromData(data[i]);
-        
+
         return returnPacket;
     }
-    
-    public static void exportAsJson(Data[] data) {
+
+    public static void exportAsJson(Data[][] data) {
         String tab = "    ";
         String jsonString = "[\n" + tab + "[\n" + (tab+tab);
-        int timePos = 0;
-        int i = 0;
-        jsonString += "\'Series\', [";
-        while(i < data.length) {
-            jsonString += data[i].locationCoords.latitude + ", ";
-            jsonString += data[i].locationCoords.longitude + ", ";
-            jsonString += 10;
-            if((i+1) < data.length)
-                jsonString += ", "; 
-            i++;
+        for(Data[] set : data) {
+            int i = 0;
+            jsonString += "\'Series\', [";
+            while(i < data.length) {
+                jsonString += data[i].locationCoords.latitude + ", ";
+                jsonString += data[i].locationCoords.longitude + ", ";
+                jsonString += 10;
+                if((i+1) < data.length)
+                    jsonString += ", ";
+                i++;
+            }
         }
-        jsonString += "]\n" + tab + "]\n" + "]";   
+        jsonString += "]\n" + tab + "]\n" + "]";
         File jsonFile = new File("resources", "data.json");
-        try { 
+        try {
             PrintWriter jsonWriter = new PrintWriter(new BufferedWriter(new FileWriter(jsonFile)));
             jsonWriter.print(jsonString);
         } catch (IOException e) { System.err.println("Error: " + e); }
-        
+
         System.out.println(jsonString);
     }
 }
